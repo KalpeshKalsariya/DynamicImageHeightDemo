2f6a26bc889539782a14b44e82686a42eb0e5b97
KalpeshKalsariya <38584779+KalpeshKalsariya@users.noreply.github.com>
2025-04-26 15:47:17 +0530

    Update HomeCollectionViewCell.swift

--- DynamicImageHeightDemo/CollectionviewCell/HomeCollectionViewCell.swift
+++ DynamicImageHeightDemo/CollectionviewCell/HomeCollectionViewCell.swift
@@ -5,17 +5,18 @@
 //  Created by  Kalpesh on 19/04/25.
 //
 
+//
+//  HomeCollectionViewCell.swift
+//  DynamicImageHeightDemo
+//
+//  Created by  Kalpesh on 19/04/25.
+//
+
 import UIKit
 
 class HomeCollectionViewCell: UICollectionViewCell {
+    
     @IBOutlet weak var imgBKG: UIImageView!
-    var imageHeight: CGFloat = 0 {
-            didSet {
-                // Notify when the image height changes
-                NotificationCenter.default.post(name: .imageHeightChanged, object: self)
-            }
-        }
-    var indexPath: IndexPath?
     let activityIndicator = UIActivityIndicatorView(style: .medium)
     
     override func awakeFromNib() {
@@ -31,7 +32,7 @@ class HomeCollectionViewCell: UICollectionViewCell {
         ])
         
         // Set corner radius for the image view
-        self.imgBKG.layer.cornerRadius = 10  // Adjust the radius as needed
+        self.imgBKG.layer.cornerRadius = 5  // Adjust the radius as needed
         self.imgBKG.clipsToBounds = true
     }
     
@@ -39,20 +40,6 @@ class HomeCollectionViewCell: UICollectionViewCell {
         super.prepareForReuse()
         imgBKG.image = nil
         activityIndicator.stopAnimating()
-        indexPath = nil  // Clear the index path when reused
     }
     
-    // Method to update the image size
-    func updateImageSize(size: CGSize) {
-        // Set the new size for the image view
-        imgBKG.frame.size = size
-        self.imageHeight = size.height
-        
-        // Post a notification when the image height changes
-        NotificationCenter.default.post(name: .imageHeightChanged, object: self, userInfo: ["indexPath": indexPath!])
-    }
-}
-
-extension Notification.Name {
-    static let imageHeightChanged = Notification.Name("imageHeightChanged")
 }
